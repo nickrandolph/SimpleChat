bb193332076dc60e19560ac7512de846a9b7bfe0
Nick Randolph <nick@builttoroam.com>
2024-04-14 04:43:27 +1000

    feat: Adding chat

--- SimpleChat/Presentation/ChatModel.cs
+++ SimpleChat/Presentation/ChatModel.cs
@@ -1,5 +1,40 @@
+using Azure.AI.OpenAI;
+
 namespace SimpleChat.Presentation;
 
-public partial record ChatModel(Entity Entity)
+public partial record ChatModel(
+    Entity Entity,
+    IOptions<AppConfig> Config,
+    IOptions<SettingsInfo> Settings)
 {
+    public IState<string> Query => State<string>.Value(this, () => "Explain AI");
+
+    public IState<string> Response => State<string>.Value(this, () => string.Empty);
+
+    public async Task Send(string query)
+    {
+        var response = await GetChatGPTResponseAsync(query);
+        await Response.SetAsync(response);
+    }
+
+    private async Task<string> GetChatGPTResponseAsync(string query)
+    {
+        var client = new OpenAIClient(Config.Value.ApiKey);
+
+        var completionOptions = new ChatCompletionsOptions
+        {
+            DeploymentName = Settings.Value.ServiceModel,
+        };
+
+        completionOptions.Messages.Add(new ChatRequestUserMessage(query));
+
+        var resp = await client.GetChatCompletionsAsync(completionOptions);
+
+        if (resp?.Value?.Choices.FirstOrDefault() is { } response &&
+            response.Message?.Content is { } responseMessage)
+        {
+            return responseMessage;
+        }
+        return "No response";
+    }
 }
